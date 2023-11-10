889913cc8cba1fe48ce4eeec842f90cb66a8b558
LionAB <lionelabatan@gmail.com>
2023-11-10 10:51:32 +0100

    [MAJ] -Swagger change swager url

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .addTag('skinsight')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-docs/v1', app, document);
   await app.listen(3000);
 }
 bootstrap();
